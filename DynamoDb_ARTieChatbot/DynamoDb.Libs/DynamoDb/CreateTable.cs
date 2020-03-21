73a1199d6bd21cbda8bdb757b68f6d6e79524b37
Марія <norman3970@gmail.com>
2020-03-21 22:59:42 +0200

    DynamoDb Client was created. Started work with database

---
+++ DynamoDb_ARTieChatbot/DynamoDb.Libs/DynamoDb/CreateTable.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
+
+namespace DynamoDb.Libs.DynamoDb
+{
+    public class CreateTable : ICreateTable
+    {
+        private readonly IAmazonDynamoDB _dynamoDbClient;
+        private static readonly string tableName = "Table";
+
+        public CreateTable(IAmazonDynamoDB dynamoDbClient)
+        {
+            _dynamoDbClient = dynamoDbClient;
+        }
+
+        public void CreateDynamoDbTable()
+        {
+            try
+            {
+                CreateTempTable();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        private void CreateTempTable()
+        {
+            Console.WriteLine("Creating Table. Please wait...");
+
+            var request = new CreateTableRequest
+            {
+                AttributeDefinitions = new List<AttributeDefinition>
+                {
+                    new AttributeDefinition
+                    {
+                        AttributeName = "Id",
+                        AttributeType = "N"
+                    },
+                    new AttributeDefinition
+                    {
+                        AttributeName = "ReplyDateTime",
+                        AttributeType = "N"
+                    }
+                },
+                KeySchema = new List<KeySchemaElement>
+                {
+                    new KeySchemaElement
+                    {
+                        AttributeName = "Id",
+                        KeyType = "HASH"
+                    },
+                    new KeySchemaElement
+                    {
+                        AttributeName = "ReplyDateTime",
+                        KeyType = "Range"
+                    }
+                },
+                ProvisionedThroughput = new ProvisionedThroughput
+                {
+                    ReadCapacityUnits = 5,
+                    WriteCapacityUnits = 5
+                },
+                TableName = tableName
+            };
+            var responce = _dynamoDbClient.CreateTableAsync(request);
+            WaitUntilTableReady(tableName);
+        }
+
+        public void WaitUntilTableReady(string tableName)
+        {
+            string status = null;
+            do
+            {
+                Thread.Sleep(5000);
+                try
+                {
+                    var res = _dynamoDbClient.DescribeTableAsync(new DescribeTableRequest
+                    {
+                        TableName = tableName
+                    });
+
+                    status = res.Result.Table.TableStatus;
+                }
+                catch (ResourceNotFoundException)
+                {
+                    
+                }
+
+            } while (status != "ACTIVE");
+            {
+                Console.WriteLine("The table was created successfully!");
+            }
+        }
+    }
+
+    
+}
---
+++ DynamoDb_ARTieChatbot/DynamoDb.Libs/DynamoDb/PutItem.cs
@@ -0,0 +1,43 @@
+﻿using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamoDb.Libs.DynamoDb
+{
+    public class PutItem : IPutItem
+    {
+        private readonly IAmazonDynamoDB _dynamoClient;
+
+        public PutItem(IAmazonDynamoDB dynamoDB)
+        {
+            _dynamoClient = dynamoDB;
+        }
+        public async Task AddNewEntry(int id, string replyDateTime)
+        {
+            var queryRequest = RequestBuilder(id, replyDateTime);
+            await PutItemAsync(queryRequest);
+        }
+        private PutItemRequest RequestBuilder(int id, string replyDateTime)
+        {
+            var item = new Dictionary<string, AttributeValue>
+            {
+                { "Id", new AttributeValue { N = id.ToString()}},
+                { "ReplyDateTime", new AttributeValue { N = replyDateTime}}
+
+            };
+            return new PutItemRequest
+            {
+                TableName = "Table",
+                Item = item
+            };
+        }
+
+        private async Task PutItemAsync(PutItemRequest request)
+        {
+            await _dynamoClient.PutItemAsync(request);
+        }
+    }
+}
